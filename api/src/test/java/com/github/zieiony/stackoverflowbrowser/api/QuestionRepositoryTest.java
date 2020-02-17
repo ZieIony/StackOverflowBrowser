e33c2101aaecbb1380b5bb1d58f21157ab532c75
Zielony <niewartotupisac@gmail.com>
2020-02-17 13:37:31 +0100

    added missing repository test

--- api/src/test/java/com/github/zieiony/stackoverflowbrowser/api/QuestionRepositoryTest.java
+++ api/src/test/java/com/github/zieiony/stackoverflowbrowser/api/QuestionRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.github.zieiony.stackoverflowbrowser.api;
 
+import com.github.zieiony.stackoverflowbrowser.api.data.Answer;
+import com.github.zieiony.stackoverflowbrowser.api.data.AnswersResponse;
 import com.github.zieiony.stackoverflowbrowser.api.data.Question;
 import com.github.zieiony.stackoverflowbrowser.api.data.QuestionsResponse;
 import com.github.zieiony.stackoverflowbrowser.api.web.SortingOrder;
@@ -20,11 +22,12 @@ import io.reactivex.Observable;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
-public class UserDataRepositoryTest {
+public class QuestionRepositoryTest {
 
     private QuestionRepository questionRepository;
 
@@ -46,10 +49,25 @@ public class UserDataRepositoryTest {
         questionsResponse.setItems(questions);
         given(stackOverflowAPI.searchQuestions(anyString(), anyInt(), anyInt(), any(SortingOrder.class), any(SortingType.class), anyString(), anyString())).willReturn(Observable.just(questionsResponse));
 
-        Observable<QuestionsResponse> responseObservable = questionRepository.getQuestions("java", 5);
-        QuestionsResponse response1 = responseObservable.blockingFirst();
-        assert Arrays.equals(response1.getItems(), questions);
+        QuestionsResponse response = questionRepository.getQuestions("java", 5).blockingFirst();
+        assert Arrays.equals(response.getItems(), questions);
 
         verify(stackOverflowAPI).searchQuestions(anyString(), anyInt(), anyInt(), any(SortingOrder.class), any(SortingType.class), anyString(), anyString());
+    }
+
+    @Test
+    public void testGetAnswers() {
+        Answer[] answers = {
+                new Answer(),
+                new Answer()
+        };
+        AnswersResponse answersResponse = new AnswersResponse();
+        answersResponse.setItems(answers);
+        given(stackOverflowAPI.requestAnswers(anyLong(), anyInt(), anyInt(), any(SortingOrder.class), any(SortingType.class), anyString(), anyString())).willReturn(Observable.just(answersResponse));
+
+        AnswersResponse response = questionRepository.getAnswers(0L, 5).blockingFirst();
+        assert Arrays.equals(response.getItems(), answers);
+
+        verify(stackOverflowAPI).requestAnswers(anyLong(), anyInt(), anyInt(), any(SortingOrder.class), any(SortingType.class), anyString(), anyString());
     }
 }
